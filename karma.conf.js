a47dd118a28bd65b3b8239e296ae249cba59b95f
tolabey <tolamusti@gmail.com>
2018-04-17 17:19:22 +0300

    fixing test directory

--- karma.conf.js
+++ karma.conf.js
@@ -5,12 +5,12 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: './src/test/',
 
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai', 'browserify', "enzyme"],
+    frameworks: ['mocha', 'chai', 'browserify'],
 
 
     // list of files / patterns to load in the browser
---
+++ src/test/App.test.js
@@ -0,0 +1,33 @@
+
+var chai = require("chai");
+
+var assert = chai.assert;
+
+
+it("renders without crashing", () => {
+  assert.equal(true, true);
+});
+
+it("renders without crashing", () => {
+  assert.equal(true, true);
+});
+
+it("renders without crashing", () => {
+  assert.equal(true, true);
+});
+
+it("renders without crashing", () => {
+  assert.equal(true, true);
+});
+
+it("renders without crashing", () => {
+  assert.equal(true, true);
+});
+
+it("renders without crashing", () => {
+  assert.equal(true, true);
+});
+
+it("renders without crashing", () => {
+  assert.equal(true, true);
+});
